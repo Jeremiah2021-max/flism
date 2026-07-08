79c689df36bce4200bda841873c92b5dd094fe68
Jeremiah Mensah <jerryjkm123@gmail.com>
2026-07-08 16:11:20 +0000

    Add Render root entrypoint and start script fix

---
+++ server.js
@@ -0,0 +1,3 @@
+// Root compatibility entrypoint for Render and other default Node deployments.
+// This forwards execution to the real app entrypoint in server/index.js.
+require("./server/index.js");
